07d302117d02a8cc0e2391043b69cc7ef3235b8e
Diogo <diogof-6@hotmail.com>
2021-01-17 19:00:21 +0100

    add hint and interactions with hint button

--- lib/widgets/game_screen.dart
+++ lib/widgets/game_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class GameScreen extends StatefulWidget {
@@ -10,6 +11,12 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreen extends State<GameScreen> {
+  double _hinOpacity = 0;
+  void _toggleHint(){
+    setState(() {
+      _hinOpacity = 1 - _hinOpacity;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,41 +40,59 @@ class _GameScreen extends State<GameScreen> {
                 child: Column (
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
+                    hint("Hint: La Solution ultime"),
                     buildAnswersButtons(["Answer 1","Answer 2", "Answer 3"]),
                   ],
                 ),
               ),
             ),
-            buildHint(context),
+            buildHintButton(),
           ],
         ),
       ),
     );
   }
-}
-Widget buildAnswersButtons(array){
-  return Column(
-    children: [
-      ...array.map( (text) {
-        return ElevatedButton(
-          onPressed: () {
-            log("Success click: $text");
-          },
-          child: SizedBox(
-            width: double.infinity,
-            child: Text(text, textScaleFactor: 2.0, textAlign: TextAlign.center)
-          )
-        );
-      }),
-    ],
-  );
-}
-Widget buildHint(BuildContext context) {
-  return Center(
-    child: FloatingActionButton(
-      onPressed: () => log("Success click: Hint"),
-      child: Icon(Icons.help_outline),
-      backgroundColor: Theme.of(context).accentColor,
-    ),
-  );
+
+  Widget hint(text){
+    return Container(
+      padding: const EdgeInsets.only(top: 20, bottom: 15),
+      child : Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Opacity(
+            child: Text(text, textScaleFactor: 1.0, textAlign: TextAlign.center),
+            opacity: _hinOpacity,
+          ),
+        ],
+      ),
+    );
+  }
+  
+  Column buildAnswersButtons(array){
+    return Column(
+      children: [
+        ...array.map( (text) {
+          return ElevatedButton(
+              onPressed: () {
+                log("Success click: $text");
+              },
+              child: SizedBox(
+                  width: double.infinity,
+                  child: Text(text, textScaleFactor: 2.0, textAlign: TextAlign.center)
+              )
+          );
+        }),
+      ],
+    );
+  }
+
+  Widget buildHintButton() {
+    return Center(
+      child: FloatingActionButton(
+        onPressed: () => _toggleHint(),
+        child: Icon(Icons.help_outline),
+        backgroundColor: Theme.of(context).accentColor,
+      ),
+    );
+  }
 }
